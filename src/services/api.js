ba9de2239bf19c3d4ed404c5787443ea6f80af2f
Felipe Oliveira <me.felipeoliveira@gmail.com>
2022-07-02 22:23:22 -0300

    env

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = ""; // your api key here
+const API_KEY = process.env.API_KEY; // your api key here
 const getPoster = poster => "https://image.tmdb.org/t/p/original/" + poster;
 const Http = axios.create(
     {
