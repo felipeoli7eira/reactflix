9414c345c815f431b7710802a96327f97f60da74
felipeoli7eira <me.felipeoliveira@gmail.com>
2025-03-01 12:51:55 -0300

    change env access

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const API_KEY = process.env.API_KEY; // your api key here
-const getPoster = poster => "https://image.tmdb.org/t/p/original/" + poster;
-const Http = axios.create(
-    {
-        baseURL: "https://api.themoviedb.org/3"
-    }
-);
+const API_KEY = process.env.REACT_APP_API_KEY;
+const getPoster = (poster) => "https://image.tmdb.org/t/p/original/" + poster;
+const Http = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
 
 export { Http, API_KEY, getPoster };
